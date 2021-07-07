434f9c2a35aee7faf740cef6f13df0055a6106c0
hong <tnalfm12353@naver.com>
2021-07-07 18:24:22 +0900

    .

--- mysite02/src/main/java/com/douzone/mysite/web/user/UpdateFormAction.java
+++ mysite02/src/main/java/com/douzone/mysite/web/user/UpdateFormAction.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
 
 import com.douzone.mysite.repository.UserRepository;
 import com.douzone.mysite.vo.UserVo;
